be67680391181ea7d07388ac1a8939e31bc9d662
Sumedh Kulkarni <126947403+sumedhk230@users.noreply.github.com>
2023-12-07 10:08:53 +0530

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
+ 
+  return (
+    <div className="Todo">
+        <p className={`${task.completed ? 'completed' : ""}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
+        <div>
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        </div>
+    </div>
+  )
+}
